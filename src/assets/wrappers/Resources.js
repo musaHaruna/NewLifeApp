b58a5e4dddfc07f658cb38d24fd2a93158270e2b
musaHaruna <124810379+musaHaruna@users.noreply.github.com>
2023-10-25 13:26:35 -0700

    resources-styles

--- src/assets/wrappers/Resources.js
+++ src/assets/wrappers/Resources.js
@@ -1,5 +1,101 @@
 import styled from 'styled-components'
 
 const Wrapper = styled.main`
+  .tab-content {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: 2rem;
+  }
+
+  .tab-content h2 {
+    color: var(--primary-blue);
+    font-size: 30px;
+    font-weight: 600;
+  }
+
+  .search {
+    display: flex;
+    align-items: center;
+    width: 30%;
+    background-color: #fff;
+    padding: 0.5rem;
+    border-radius: 10px;
+  }
+
+  .search input {
+    width: 100%;
+    border: none;
+    font-size: 14px;
+  }
+
+  .search input:focus {
+    outline: none;
+  }
+
+  .btn-primary {
+    padding: 0.7rem 1rem;
+    display: flex;
+    align-items: center;
+    gap: 0.3rem;
+  }
+  .btn-primary button {
+    background-color: inherit;
+
+    border: none;
+    color: inherit;
+    font-size: 14px;
+    padding: 0;
+    margin: 0;
+  }
+
+  .photos-search {
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    gap: 0.5rem;
+    width: 60%;
+  }
+
+  .photos-search .search {
+    width: 60%;
+  }
+
+  .tabs {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background-color: #f2f7fd;
+    padding: 1rem;
+    border-radius: 15px;
+  }
+
+  .tabs div {
+    display: flex;
+    align-items: center;
+    color: #1e1e1e;
+  }
+  .tab-btn {
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    gap: 0.2rem;
+  }
+
+  .tab-btn h4 {
+    font-weight: 500;
+  }
+  .tab-btn.tab-active {
+    color: var(--primary-blue);
+  }
+
+  .tab-btn.active .icon {
+    color: var(--primary-blue);
+  }
+
+  .tab-btn.active h4 {
+    color: var(--primary-blue);
+  }
 `
+
 export default Wrapper
--- src/pages/dashboard/resources-page/Resources.js
+++ src/pages/dashboard/resources-page/Resources.js
@@ -1,6 +1,11 @@
 import React, { useState } from 'react'
 import Wrapper from '../../../assets/wrappers/Resources'
 import Documents from './Documents'
+import { CiSearch } from 'react-icons/ci'
+import { HiOutlinePhotograph } from 'react-icons/hi'
+import { HiOutlineDocumentText } from 'react-icons/hi'
+import { TiDocumentText } from 'react-icons/ti'
+import { FiLink2 } from 'react-icons/fi'
 
 const Resources = () => {
   const [activeTab, setActiveTab] = useState('Documents')
@@ -8,49 +13,84 @@ const Resources = () => {
   return (
     <Wrapper>
       {activeTab === 'Documents' && (
-        <article>
+        <article className='tab-content'>
           <h2>Documents</h2>
-          <input type='text' />
+          <div className='search'>
+            <CiSearch className='search-icon' />
+            <input type='text' placeholder='Search Documents' />
+          </div>
         </article>
       )}
       {activeTab === 'Photos' && (
-        <article>
+        <article className='tab-content photos'>
           <h2>Photos</h2>
-          <input type='text' />
+          <div className='photos-search'>
+            <div className='search'>
+              <CiSearch className='search-icon' />
+              <input type='text' placeholder='Search Photos' />
+            </div>
+            <div className='btn-primary '>
+              <HiOutlinePhotograph className='icon' />
+              <button>Add Photos</button>
+            </div>
+          </div>
         </article>
       )}
       {activeTab === 'Useful Links' && (
-        <article>
+        <article className='tab-content'>
           <h2>Useful Links</h2>
-          <input type='text' />
+          <div className='btn-primary '>
+            <FiLink2 className='icon' />
+            <button>Usefull Links</button>
+          </div>
         </article>
       )}
       {activeTab === 'Publications and Research' && (
-        <article>
+        <article className='tab-content'>
           <h2>Publications and Research</h2>
-          <input type='text' />
+          <div className='search'>
+            <CiSearch className='search-icon' />
+            <input type='text' placeholder='Search Documents' />
+          </div>
         </article>
       )}
 
       <article>
-        <section>
-          <div onClick={() => setActiveTab('Documents')}>
-            <p>icon</p>
+        <section className='tabs'>
+          <div
+            onClick={() => setActiveTab('Documents')}
+            className={`tab-btn ${activeTab === 'Documents' ? 'active' : ''}`}
+          >
+            <HiOutlineDocumentText className='icon' />
             <h4>Documents</h4>
           </div>
-          <div onClick={() => setActiveTab('Photos')}>
-            <p>icon</p>
+          <div
+            onClick={() => setActiveTab('Photos')}
+            className={`tab-btn ${activeTab === 'Photos' ? 'active' : ''}`}
+          >
+            <HiOutlinePhotograph className='icon' />
             <h4>Photos</h4>
           </div>
-          <div onClick={() => setActiveTab('Useful Links')}>
-            <p>icon</p>
+          <div
+            onClick={() => setActiveTab('Useful Links')}
+            className={`tab-btn ${
+              activeTab === 'Useful Links' ? 'active' : ''
+            }`}
+          >
+            <FiLink2 className='icon' />
             <h4>Useful Links</h4>
           </div>
-          <div onClick={() => setActiveTab('Publications and Research')}>
-            <p>icon</p>
+          <div
+            onClick={() => setActiveTab('Publications and Research')}
+            className={`tab-btn ${
+              activeTab === 'Publications and Research' ? 'active' : ''
+            }`}
+          >
+            <TiDocumentText className='icon' />
             <h4>Publications and Research</h4>
           </div>
         </section>
+
         <section>
           {activeTab === 'Documents' && <Documents />}
           {activeTab === 'Photos' && 'Photos'}

08a1e6d5d6678951c28df4f9e15728261134fe0e
musaHaruna <124810379+musaHaruna@users.noreply.github.com>
2024-03-09 07:46:49 -0800

    get-photos

--- src/components/resources-page/Photos.js
+++ src/components/resources-page/Photos.js
@@ -1,45 +1,23 @@
 import photo from '../../assets/images/photo.png'
+import { useQuery } from '@tanstack/react-query'
+import user from '../../services/api/user'
 
 const Photos = () => {
+  const getPhotos = useQuery({
+    queryKey: ['get-photos'],
+    queryFn: user.getPhotos,
+  })
+  const photos = getPhotos?.data?.photos
+  console.log(photos)
+
   return (
     <article className='tcontainer-wrapper photo'>
       <section className='photo-grid'>
-        <div>
-          <img src={photo} alt='picture' />
-        </div>
-        <div>
-          <img src={photo} alt='picture' />
-        </div>
-        <div>
-          <img src={photo} alt='picture' />
-        </div>
-        <div>
-          <img src={photo} alt='picture' />
-        </div>
-        <div>
-          <img src={photo} alt='picture' />
-        </div>
-        <div>
-          <img src={photo} alt='picture' />
-        </div>
-        <div>
-          <img src={photo} alt='picture' />
-        </div>
-        <div>
-          <img src={photo} alt='picture' />
-        </div>
-        <div>
-          <img src={photo} alt='picture' />
-        </div>
-        <div>
-          <img src={photo} alt='picture' />
-        </div>
-        <div>
-          <img src={photo} alt='picture' />
-        </div>
-        <div>
-          <img src={photo} alt='picture' />
-        </div>
+        {photos?.map((photo, index) => (
+          <div key={index}>
+            <img src={photo.file.url} alt={`picture-${index}`} />
+          </div>
+        ))}
       </section>
       <div className='tbtn-container'>
         <button className='btn-primary load-more'>Load More</button>

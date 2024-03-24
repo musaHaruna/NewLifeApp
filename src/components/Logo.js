6585a6bcc2f1a1c3c6bc5facdc9893aa57753fe5
musaHaruna <124810379+musaHaruna@users.noreply.github.com>
2024-03-24 13:14:16 -0700

    logo-change

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,6 +1,6 @@
 import logo from '../assets/images/Logo.png'
 
 const Logo = () => {
-  return <img src={logo} alt='jobster logo' width={60} />
+  return <img src={logo} alt='jobster logo' width={50} />
 }
 export default Logo
